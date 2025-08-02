58a0d87071eab4789d9a37e9ce55ce80cd742060
xXNCSXx <51480995+xXNCSXx@users.noreply.github.com>
2025-08-02 08:40:24 -0300

    2

---
+++ SITE_PORT3/node_modules/@react-three/flex/dist/hooks.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+export declare function useContext<T>(context: React.Context<T>): T;
+export declare function useReflow(): () => void;
+export declare function useFlexSize(): [number, number];
